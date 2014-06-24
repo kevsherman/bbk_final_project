cb6bd66f964c2d8b4fd1e8ef4607023a1b52f1a2
Kevin Sherman <kevsherman@gmail.com>
2014-06-24 14:48:35 -0700

    completed user validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   has_many :main_events
   has_secure_password
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: {with: VALID_EMAIL_REGEX }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,11 +7,25 @@ describe User do
   end
 
   it "should have an email, password and password confirmation" do 
-    expect(:user).to be_valid
+    user = FactoryGirl.build(:user)
+    expect(user).to be_valid
   end
 
   it "should not accept an empty name field" do
-    user = User.new(email: " ")
+    user = FactoryGirl.build(:user)
+    user.email = " "
+    expect(user).not_to be_valid
+  end
+
+  it "should not allow dupilicate emails" do
+    user1 = create(:user)
+    user2 = build(:user)
+    expect(user2).not_to be_valid
+  end
+
+  it "should only allow proper email format" do
+    user = build(:user)
+    user.email = "nopenopenope"
     expect(user).not_to be_valid
   end
 end
