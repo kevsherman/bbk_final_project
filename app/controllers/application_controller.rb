8ae9b840f8800935ccac38b4ee69e4ed71f3ca4b
Kevin Sherman <kevsherman@gmail.com>
2014-06-26 17:22:41 -0700

    clean up application contrller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,4 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def formatted_date(date)
-   date
-  end
-
 end
