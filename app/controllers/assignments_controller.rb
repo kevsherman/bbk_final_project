9b7a9f8ff6788e0a8585944d06fe350ad5527caa
Kevin Sherman <kevsherman@gmail.com>
2014-06-27 18:40:56 -0700

    guests can be assigned to a subevent from the subevent show page

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -2,12 +2,19 @@ class AssignmentsController < ApplicationController
   def new
     @main_event = MainEvent.where(user_id: session[:id])
     @sub_event = SubEvent.find(params[:sub_event_id])
-    @guests = Guest.where(main_event_id: params[:main_event_id])
+    
     @assignment = Assignment.new
   end
 
   def create
-    
+    @guest_ids = params[:assignment][:guest_id]
+    @guest_ids.each do |g_id|
+      new_assignment = Assignment.new(guest_id: g_id, sub_event_id: params[:sub_event_id])
+      if new_assignment.guest_id
+        new_assignment.save
+      end
+    end
+    redirect_to :back
   end
 
   
