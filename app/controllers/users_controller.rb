c1f7404e3ad6696d3e4317094715bd25abfffe01
Kevin Sherman <kevsherman@gmail.com>
2014-07-02 11:32:48 -0700

    user name in navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :firstname, :lastname)
   end
 
 end
