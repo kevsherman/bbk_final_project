54546405449b8ff82055b250abc1e8312c5d75bf
Brett Campbell <campbell.b@live.ca>
2014-07-02 22:07:05 +0000

    Redirect to Thankyou After Guest Assignment Update

--- app/controllers/itinerary_controller.rb
+++ app/controllers/itinerary_controller.rb
@@ -1,12 +1,9 @@
 class ItineraryController < ApplicationController
-
   def show 
     @guest = Guest.find(params[:id])
     @assignment = Assignment.new
     @main_event = MainEvent.where(id: @guest[:main_event_id]).first
   end
 
-  def thankyou
-   render '/thankyou'
-  end
+  
 end
