f63c6ca75933c6f8583593d151b44395a7f9eb47
Kevin Sherman <kevsherman@gmail.com>
2014-07-05 20:48:51 -0700

    mailer fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,12 @@ class UserMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
     @url = 'http://example.com/login'
-    mail(to: @user.email, from: "BlackTie.com", subject: 'Welcome to BlackTie!')
+    mail(to: @user.email, subject: 'Welcome to BlackTie!')
   end
 
   def invite_guests(guest, main_event)
     @guest = guest
     @main_event = main_event
-    mail(to: guest.email, from: "BlackTie.com", subject: "You've been invited to #{@main_event.title}!")
+    mail(to: guest.email, from: "invites@blacktie.com", subject: "you've been invited to #{@main_event.title}")
   end
-
-
 end
