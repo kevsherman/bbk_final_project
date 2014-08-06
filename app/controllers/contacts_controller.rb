3bed02f37719b65ee7234bdf06f5338a439db29e
hombredestutz <barrett.stutzman@gmail.com>
2014-08-05 19:46:25 -0700

    updating for Gabe pull request

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,6 +2,7 @@ class ContactsController < ApplicationController
 
   def index
     @contacts=request.env['omnicontacts.contacts']
+    @main_event=main_event
   end
 
   def failure
--- app/models/main_event.rb
+++ app/models/main_event.rb
@@ -2,6 +2,7 @@ class MainEvent < ActiveRecord::Base
   belongs_to :user
   has_many :sub_events
   has_many :guests
+  accepts_nested_attributes_for :guests
 
   validates_presence_of :user_id, :title, :groom_full_name, :bride_full_name
   validates_numericality_of :user_id, {only_integer: true}
